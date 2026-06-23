a79a0c29a5f0048f12fea46c18d130eaa092c99e
Mark0548Addo <markosroagbo12@gmail.com>
2026-06-23 15:05:39 +0000

    this my assignment for week 4

---
+++ Mark.java
@@ -0,0 +1,41 @@
+public class Mark {
+
+    static void main() {
+        String myName = "Mark Kwei Addo";
+        char myGender ='M';
+        int  age = 32;
+        short  myShoeSize =45;
+         long myContact =+2330548124580L;
+         float  myAccount = 35.4700f;
+         double  myHeight = 5.75;
+         boolean isExcellent =true;
+
+         //using the printf method to print the output
+
+        System.out.printf("My name is " +myName );
+        System.out.printf(" and i am a male " +myGender+","  );
+        System.out.printf(" I am "+age+ "years old.");
+        System.out.printf("\n My shoe size is " +myShoeSize  );
+        System.out.printf("\n My contact number is " +myContact  );
+        System.out.printf("\n My bank account balance is " +myAccount);
+        System.out.printf("\n I have a height of " +myHeight+ "feet");
+        System.out.printf(" \nI am the over all best student on  KTU campus "+isExcellent );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
